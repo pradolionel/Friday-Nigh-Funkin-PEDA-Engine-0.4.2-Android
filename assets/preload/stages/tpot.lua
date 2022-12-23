d08fd6b520ed51ac01f6793b84d7b889c27c3640
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-12-22 19:06:21 -0500

    Delete tpot.lua

--- assets/preload/stages/tpot.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-	-- background shit
-	makeLuaSprite('tpot', 'tpot', -230, -300);
-	setScrollFactor('tpot', 0.9, 0.9);
-
-	addLuaSprite('tpot', false);
-	
-	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
-end
